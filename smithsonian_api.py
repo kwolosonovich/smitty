5b2e43c9829046687ce3b6076c4bb0b24c5b07fb
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-07-26 15:25:19 -0700

    updated query string; implemented class variables and functions

--- smithsonian_api.py
+++ smithsonian_api.py
@@ -9,37 +9,42 @@ from secure import api_key
 
 API_BASE_URL = 'https://api.si.edu/openaccess/api/v1.0/search'
 
-
-def get_images(keyword=None):
-    """Request random images from smithsonian API for homepage"""
-    # test API requests
-   
-    # request using keyword
-    q = "edward-hopper"
-    rows_ = 1000
-    type = "images"
-
-    # search for items
-    resp = requests.get(url=API_BASE_URL,
-                        params={"q": q,
-                                "api_key": api_key,
-                                "rows": rows_})
-
-    # iterate through the items for image URL
-    rows = resp.json()["response"]["rows"]
-    image_urls = []
-    for row in rows:
-        # if "online_media" in row["content"]["descriptiveNonRepeating"].keys():
-        #     if "resources" in row["content"]["descriptiveNonRepeating"]["online_media"]["media"][0].keys():
-        #         url = row["content"]["descriptiveNonRepeating"]["online_media"]["media"][0]["resources"][0]["url"]
-        #         image_urls.append(url)
-                
-
-        if "online_media" in row["content"]["descriptiveNonRepeating"]:
-            online_media = row["content"]["descriptiveNonRepeating"].get('online_media')
-            if online_media and online_media["media"][0].get("resources"):
-                url = online_media["media"][0]["resources"][0]["url"]
-                image_urls.append(url)        
+class ApiImage:
+    """Create API image."""
+    def __init__(self, url, title, artist, date, medium, collection):
         
-    
-    return image_urls
+        self.url = url
+        self.title = title
+        self.artist = artist
+        self.date = date
+        self.medium = medium 
+        self.collection = collection
+        
+    def get_images(search=None):
+        """Request random images from smithsonian API for homepage"""
+        # test API requests
+
+        # request using keyword
+        q = 'data_source="American Art"&online_media_type=images&images=jpeg'
+        rows_ = 1
+
+        # search for items
+        resp = requests.get(url=API_BASE_URL,
+                            params={"q": q,
+                                    "api_key": api_key,
+                                    "rows": rows_})
+        
+        # iterate through the items for image URL
+        rows = resp.json()["response"]["rows"]
+        for row in rows:
+            if "online_media" in row["content"]["descriptiveNonRepeating"]:
+
+                url = rows[0]["content"]["descriptiveNonRepeating"]["online_media.media"][0]["resources"][2]["url"]
+                title = rows[0]["content"]["descriptiveNonRepeating"]["title"]["content"]
+                artist = rows[0]["content"]["descriptiveNonRepeating"]["title"]["content"]
+                date = rows[0]["content"]["freetext"]["date"][0]["content"]
+                medium = rows[0]["content"]["freetext"]["physicalDescription"][0]["content"]
+                collection = rows[0]["content"]["freetext"]["setName"][0]["content"]
+
+                ApiImage(url, title, artist, date, collection, date, medium)
+
